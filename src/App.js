6adfa143a912bc7b35fa20b39eb7b12209b80e03
Sergio Can <gersiopumas@gmail.com>
2021-10-16 17:41:25 -0500

    change name by the one provided by auth0

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default () => {
               } 
               return (
                 <route.layout {...props}>
-                  <route.component {...props} />
+                  <route.component {...props} user={user}  />
                 </route.layout>
               );
             })}
