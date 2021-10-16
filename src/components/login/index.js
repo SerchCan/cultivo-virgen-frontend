5e4c551fbe72d4f10a8fec647451f22da7819944
jipixz <gibranjramon@gmail.com>
2021-10-16 18:17:28 -0500

    First thingy

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "shards-react";
+
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Click aquí para iniciar sesión</button>;
+  return (
+    <div className="d-flex justify-content-center">
+      <Button 
+        className="btn" pill 
+        onClick={() => loginWithRedirect()}
+      >
+        Click aquí para iniciar sesión
+      </Button>
+    </div>
+  )
+
 };
 
 export default LoginButton;
