b2e9bbe1622092f74f35acda2388b048133ea97a
Sergio Can <gersiopumas@gmail.com>
2021-11-02 17:01:42 -0500

    change build script base and change api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:4000/";
+axios.defaults.baseURL = "https://api.cultivo-virgen.link/";
 
 const api = {
-  getFromEndpoint: async(endpoint) => await axios.get(endpoint),
-  putFromEndpoint: async(endpoint,params={}) => await axios.put(endpoint,params),
-  postFromEndpoint: async(endpoint,params={}) => await axios.post(endpoint,params),
-  deleteFromEndpoint: async(endpoint,params={}) => await axios.delete(endpoint,params),
-  postProduct: async()=> await axios.post('/product'),
-  getProducts: async()=> await axios.get('/product/find'),
+  getFromEndpoint: async (endpoint) => await axios.get(endpoint),
+  putFromEndpoint: async (endpoint, params = {}) => await axios.put(endpoint, params),
+  postFromEndpoint: async (endpoint, params = {}) => await axios.post(endpoint, params),
+  deleteFromEndpoint: async (endpoint, params = {}) => await axios.delete(endpoint, params),
+  postProduct: async () => await axios.post('/product'),
+  getProducts: async () => await axios.get('/product/find'),
 
-  saveLogbook: async(params={})=> await axios.post('/logbook/save',params),
-  getLogbook: async(params={})=> await axios.post('/logbook/get',params),
+  saveLogbook: async (params = {}) => await axios.post('/logbook/save', params),
+  getLogbook: async (params = {}) => await axios.post('/logbook/get', params),
 }
 export default api;
